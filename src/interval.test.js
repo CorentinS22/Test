5038c150d4ed0a99937740f5ba79ad6204ab83af
CorentinS22 <corentin.simon@ensiie.fr>
2019-09-26 17:07:59 +0200

    finito

---
+++ src/interval.test.js
@@ -0,0 +1,17 @@
+const Interval = require('./interval.js');
+
+describe('Overlaps', function () {
+
+    test('Test interval true', () => {
+        let interval1 = new Interval(0,24);
+        let interval2 = new Interval(6,34);
+        expect(interval1.overlaps(interval2)).toBe(true);
+    });
+
+    test('Test interval false', () => {
+        let interval1 = new Interval(0,24);
+        let interval2 = new Interval(25,46);
+        expect(interval1.overlaps(interval2)).toBe(false);
+    });
+
+});
