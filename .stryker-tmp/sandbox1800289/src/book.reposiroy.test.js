db66f2dc421f75ee1b9daa8770f5f3315abeb0e7
CorentinS22 <corentin.simon@ensiie.fr>
2019-09-29 17:53:10 +0200

    MAJ

---
+++ .stryker-tmp/sandbox1800289/src/book.reposiroy.test.js
@@ -0,0 +1,32 @@
+const BookRepository = require('./book.repository');
+
+describe('Book repository Save', function () {
+
+    test('Test save a book', () => {
+
+        const dbMock = {
+            get : jest.fn().mockReturnThis(),
+            push : jest.fn().mockReturnThis(),
+            write : jest.fn().mockReturnThis()
+        };
+        const repository = new BookRepository(dbMock);
+        repository.save({id: 1, name: "Unit test"});
+
+        expect(dbMock.write.mock.calls.length).toBe(1);
+    });
+});
+
+describe('Book repository getTotalCount', function () {
+
+    test('Test getTotalCount', () => {
+
+        const dbMock = {
+            get : jest.fn().mockReturnThis(),
+            size : jest.fn().mockReturnThis(),
+            value : jest.fn().mockReturnValue(2)
+        };
+        const repository = new BookRepository(dbMock);
+
+        expect(repository.getTotalCount()).toBe(2);
+    });
+});
