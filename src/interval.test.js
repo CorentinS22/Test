995025d6cdebf0a01672975d6b99d17f0d51c3df
CorentinS22 <corentin.simon@ensiie.fr>
2019-09-30 18:04:29 +0200

    fin overlaps

--- src/interval.test.js
+++ src/interval.test.js
@@ -2,16 +2,29 @@ const Interval = require('./interval.js');
 
 describe('Overlaps', function () {
 
-    test('Test interval true', () => {
-        let interval1 = new Interval(0,24);
-        let interval2 = new Interval(6,34);
+    let interval1, interval2;
+
+    test('Test interval true / interval1 contenu dans interval2', () => {
+        interval1 = new Interval(3,6);
+        interval2 = new Interval(4,5);
         expect(interval1.overlaps(interval2)).toBe(true);
-        //test
+    });
+
+    test('Test interval true / interval1 contenu (debut) dans interval2', () => {
+        interval1 = new Interval(3,6);
+        interval2 = new Interval(4,8);
+        expect(interval1.overlaps(interval2)).toBe(true);
+    });
+
+    test('Test interval true / interval1 contenu (fin) dans interval2', () => {
+        interval1 = new Interval(6,10);
+        interval2 = new Interval(4,8);
+        expect(interval1.overlaps(interval2)).toBe(true);
     });
 
     test('Test interval false', () => {
-        let interval1 = new Interval(0,24);
-        let interval2 = new Interval(25,46);
+        interval1 = new Interval(3,6);
+        interval2 = new Interval(7,9);
         expect(interval1.overlaps(interval2)).toBe(false);
     });
 
